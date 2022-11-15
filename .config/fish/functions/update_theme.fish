12faf331050ec582e2193edbf80bd1c89516ea88
Christopher Kahn <92762809+kahnclusions@users.noreply.github.com>
2022-11-15 22:02:58 +0000

    Updating theme

---
+++ .config/fish/functions/update_theme.fish
@@ -0,0 +1,25 @@
+
+function set_theme_dark
+  echo "setting dark theme"
+  set -Ux FZF_DEFAULT_OPTS "\
+  --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+  --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+end
+
+funciton set_theme_light
+echo "setting light theme"
+  set -Ux FZF_DEFAULT_OPTS "\
+  --color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+  --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+  --color=marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39"
+end
+
+function update_theme --on-variable macOS_Theme
+  echo "theme updated"
+    if [ "$macOS_Theme" = "dark" ]
+        set_theme_dark
+    else if [ "$macOS_Theme" = "light" ]
+        set_theme_light
+    end
+end
