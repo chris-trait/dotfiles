e4adf16afd7d9314de61e97b0df26a2089a79459
Christopher Kahn <92762809+kahnclusions@users.noreply.github.com>
2022-11-23 08:25:37 +0000

    Add catppuccin fish themes

---
+++ .config/fish/themes/Catppuccin Latte.fish
@@ -0,0 +1,26 @@
+# name: 'Catppuccin latte' 
+# url: 'https://github.com/catppuccin/fish'
+# preferred_background: eff1f5
+
+set -U fish_color_normal 4c4f69
+set -U fish_color_command 1e66f5
+set -U fish_color_param dd7878
+set -U fish_color_keyword d20f39
+set -U fish_color_quote 40a02b
+set -U fish_color_redirection ea76cb
+set -U fish_color_end fe640b
+set -U fish_color_error d20f39
+set -U fish_color_gray 9ca0b0
+set -U fish_color_selection --background=ccd0da
+set -U fish_color_search_match --background=ccd0da
+set -U fish_color_operator ea76cb
+set -U fish_color_escape dd7878
+set -U fish_color_autosuggestion 9ca0b0
+set -U fish_color_cancel d20f39
+set -U fish_color_cwd df8e1d
+set -U fish_color_user 179299
+set -U fish_color_host 1e66f5
+set -U fish_pager_color_progress 9ca0b0
+set -U fish_pager_color_prefix ea76cb
+set -U fish_pager_color_completion 4c4f69
+set -U fish_pager_color_description 9ca0b0
