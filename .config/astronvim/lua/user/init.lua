631f4918354f9ab4f6a3149c21320c2edd75e050
Christopher Kahn <92762809+kahnclusions@users.noreply.github.com>
2023-03-27 21:34:57 +0100

    Update plugins

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -234,9 +234,6 @@ local config = {
                 "nvim-telescope/telescope.nvim",
                 "neovim/nvim-lspconfig",
             },
-            config = function()
-                require("xbase").setup({}) -- see default configuration bellow
-            end,
         },
         -- All other entries override the require("<key>").setup({...}) call for default plugins
         ["null-ls"] = function(config) -- overrides `require("null-ls").setup(config)`
@@ -301,36 +298,6 @@ local config = {
                 require("user.plugin.dap")
             end,
         })
-
-        function serializeTable(val, name, skipnewlines, depth)
-            skipnewlines = skipnewlines or false
-            depth = depth or 0
-
-            local tmp = string.rep(" ", depth)
-
-            if name then tmp = tmp .. name .. " = " end
-
-            if type(val) == "table" then
-                tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
-
-                for k, v in pairs(val) do
-                    tmp = tmp ..
-                        serializeTable(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
-                end
-
-                tmp = tmp .. string.rep(" ", depth) .. "}"
-            elseif type(val) == "number" then
-                tmp = tmp .. tostring(val)
-            elseif type(val) == "string" then
-                tmp = tmp .. string.format("%q", val)
-            elseif type(val) == "boolean" then
-                tmp = tmp .. (val and "true" or "false")
-            else
-                tmp = tmp .. "\"[inserializeable datatype:" .. type(val) .. "]\""
-            end
-
-            return tmp
-        end
 
         -- vim.o.background = "dark"
         -- local colorscheme = require("zenbones")
