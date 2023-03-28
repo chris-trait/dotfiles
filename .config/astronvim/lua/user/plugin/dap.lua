5ebf80884f0d20fe6eb26b5ae87224251d235eed
Christopher Kahn <92762809+kahnclusions@users.noreply.github.com>
2023-03-28 16:31:42 +0100

    Update nvim plugins

--- .config/astronvim/lua/user/plugin/dap.lua
+++ .config/astronvim/lua/user/plugin/dap.lua
@@ -1,28 +1,28 @@
 require("dap-vscode-js").setup({
-    debugger_path = require("user.extra")["dap-vscode-js"].debugger_path,
-    adapters = {
-        "pwa-node",
-        "pwa-chrome",
-        "node-terminal",
-        "pwa-extensionHost",
-    },
+   debugger_path = require("user.extra")["dap-vscode-js"].debugger_path,
+   adapters = {
+      "pwa-node",
+      "pwa-chrome",
+      "node-terminal",
+      "pwa-extensionHost",
+   },
 })
 
 for _, language in ipairs({ "typescriptreact", "typescript", "javascript" }) do
    require("dap").configurations[language] = {
-       {
-           type = "pwa-node",
-           request = "launch",
-           name = "Launch file",
-           program = "${file}",
-           cwd = "${workspaceFolder}",
-       },
-       {
-           type = "pwa-node",
-           request = "attach",
-           name = "Attach",
-           processId = require("dap.utils").pick_process,
-           cwd = "${workspaceFolder}",
-       },
+      {
+         type = "pwa-node",
+         request = "launch",
+         name = "Launch file",
+         program = "${file}",
+         cwd = "${workspaceFolder}",
+      },
+      {
+         type = "pwa-node",
+         request = "attach",
+         name = "Attach",
+         processId = require("dap.utils").pick_process,
+         cwd = "${workspaceFolder}",
+      },
    }
 end
