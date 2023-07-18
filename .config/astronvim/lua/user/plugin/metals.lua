12c877dbbf2d17e641441a90a0b10898ddc4c5de
Christopher Kahn <92762809+kahnclusions@users.noreply.github.com>
2023-07-18 10:06:57 +0100

    Update with rose-pine

--- .config/astronvim/lua/user/plugin/metals.lua
+++ .config/astronvim/lua/user/plugin/metals.lua
@@ -2,9 +2,10 @@ local api = vim.api
 local metals_config = require("metals").bare_config()
 
 metals_config.settings = {
-   showImplicitArguments = true,
+    showImplicitArguments = true,
 }
 
+metals_config.init_options.statusBarProvider = "on"
 metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 metals_config.on_attach = function()
@@ -13,9 +14,9 @@ end
 
 local nvim_metals_group = api.nvim_create_augroup("nvim-metals", { clear = true })
 api.nvim_create_autocmd("FileType", {
-   pattern = { "scala", "sbt", "java" },
-   callback = function()
-      require("metals").initialize_or_attach(metals_config)
-   end,
-   group = nvim_metals_group,
+    pattern = { "scala", "sbt", "java" },
+    callback = function()
+       require("metals").initialize_or_attach(metals_config)
+    end,
+    group = nvim_metals_group,
 })
--- .config/wezterm/colors.lua
+++ .config/wezterm/colors.lua
@@ -1,56 +1,8 @@
 local wezterm = require("wezterm")
 
 local M = {}
-function M.select(palettes, current)
-	local colors = palettes[current]
-	return {
-		foreground = colors.fg,
-		background = colors.bg,
-		cursor_bg = colors.fg,
-		cursor_border = colors.fg,
-		cursor_fg = colors.bg,
-		selection_bg = colors.grey[1],
-		selection_fg = colors.fg,
-		ansi = { colors.bg, colors.red.base, colors.green.base, colors.yellow.base, colors.blue.base, colors.magenta.base,
-			colors.cyan.base, colors.fg },
-		brights = { colors.bg, colors.red.bright, colors.green.bright, colors.yellow.bright, colors.blue.bright,
-			colors.magenta.bright, colors.cyan.bright, colors.fg },
-		-- ansi = { "#1C1917", "#DE6E7C", "#819B69", "#B77E64", "#6099C0", "#B279A7", "#66A5AD", "#B4BDC3" },
-		-- brights = { "#403833", "#E8838F", "#8BAE68", "#D68C67", "#61ABDA", "#CF86C1", "#65B8C1", "#888F94" },
-		tab_bar = {
-			background = colors.bg,
-			active_tab = {
-				fg_color = colors.beige[9],
-				bg_color = colors.beige[5],
-			},
-			inactive_tab = {
-				bg_color = colors.beige[2],
-				fg_color = colors.beige[9],
-			},
-			inactive_tab_hover = {
-				bg_color = colors.beige[4],
-				fg_color = colors.beige[9],
-			},
-			new_tab = {
-				bg_color = colors.bg,
-				fg_color = colors.fg,
-			},
-			new_tab_hover = {
-				bg_color = colors.grey[1],
-				fg_color = colors.fg,
-				italic = true,
-			},
-		},
-		visual_bell = colors.grey[1],
-		indexed = {
-			[16] = colors.yellow.bright,
-			[17] = colors.yellow.bright,
-		},
-		scrollbar_thumb = colors.grey[1],
-		split = colors.bg,
-		-- nightbuild only
-		compose_cursor = colors.yellow.bright,
-	}
+function M.select(colors, current)
+	return colors[current]
 end
 
 -- utility functions for interacting with wezterm API
@@ -89,22 +41,22 @@ local function format_tab_title(tab, tabs, panes, config, hover, max_width)
 
 	if tab.is_active then
 		return {
-			{ Background = { Color = active.bg_color } },
-			{ Foreground = { Color = active.fg_color } },
-			{ Attribute = { Intensity = "Bold" } },
-			{ Text = title },
+				{ Background = { Color = active.bg_color } },
+				{ Foreground = { Color = active.fg_color } },
+				{ Attribute = { Intensity = "Bold" } },
+				{ Text = title },
 		}
 	elseif hover then
 		return {
-			{ Background = { Color = palette.tab_bar.inactive_tab_hover.bg_color } },
-			{ Foreground = { Color = palette.tab_bar.inactive_tab_hover.fg_color } },
-			{ Text = title },
+				{ Background = { Color = palette.tab_bar.inactive_tab_hover.bg_color } },
+				{ Foreground = { Color = palette.tab_bar.inactive_tab_hover.fg_color } },
+				{ Text = title },
 		}
 	else
 		return {
-			{ Background = { Color = inactive.bg_color } },
-			{ Foreground = { Color = inactive.fg_color } },
-			{ Text = title },
+				{ Background = { Color = inactive.bg_color } },
+				{ Foreground = { Color = inactive.fg_color } },
+				{ Text = title },
 		}
 	end
 end
@@ -116,9 +68,9 @@ function M.setup(options)
 	end
 
 	options = {
-		sync = should_sync,
-		colors = options.colors,
-		mode = options.mode or "dark",
+			sync = should_sync,
+			colors = options.colors,
+			mode = options.mode or "dark",
 	}
 
 	-- https://wezfurlong.org/wezterm/config/lua/window/get_appearance.html#windowget_appearance
