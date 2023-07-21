5fd663fc3354818a326c22fbfcff664effdcb7f7
Christopher Kahn <92762809+kahnclusions@users.noreply.github.com>
2023-07-21 17:10:25 +0100

    Removing scala metals

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -12,16 +12,16 @@ end
 
 local config = {
    updater = {
-      remote = "origin", -- remote to use
-      channel = "stable", -- "stable" or "nightly"
-      version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-      branch = "nightly", -- branch name (NIGHTLY ONLY)
-      commit = nil, -- commit hash (NIGHTLY ONLY)
-      pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-      skip_prompts = false, -- skip prompts about breaking changes
+      remote = "origin",     -- remote to use
+      channel = "stable",    -- "stable" or "nightly"
+      version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+      branch = "nightly",    -- branch name (NIGHTLY ONLY)
+      commit = nil,          -- commit hash (NIGHTLY ONLY)
+      pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
+      skip_prompts = false,  -- skip prompts about breaking changes
       show_changelog = true, -- show the changelog after performing an update
-      auto_reload = false, -- automatically reload and sync packer after a successful update
-      auto_quit = false, -- automatically quit the current session after a successful update
+      auto_reload = false,   -- automatically reload and sync packer after a successful update
+      auto_quit = false,     -- automatically quit the current session after a successful update
    },
    lazy = {
       lockfile = vim.fn.stdpath("config") .. "/../astronvim/lazy-lock.json",
@@ -40,21 +40,21 @@ local config = {
       opt = {
          -- set to true or false etc.
          relativenumber = false, -- sets vim.opt.relativenumber
-         number = false, -- sets vim.opt.number
-         spell = false, -- sets vim.opt.spell
+         number = false,         -- sets vim.opt.number
+         spell = false,          -- sets vim.opt.spell
          -- signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-         wrap = true, -- sets vim.opt.wrap
+         wrap = true,            -- sets vim.opt.wrap
          showtabline = 0,
       },
       g = {
-         mapleader = " ", -- sets vim.g.mapleader
-         autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-         cmp_enabled = true, -- enable completion at start
-         autopairs_enabled = true, -- enable autopairs at start
-         diagnostics_enabled = true, -- enable diagnostics at start
+         mapleader = " ",                   -- sets vim.g.mapleader
+         autoformat_enabled = true,         -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+         cmp_enabled = true,                -- enable completion at start
+         autopairs_enabled = true,          -- enable autopairs at start
+         diagnostics_enabled = true,        -- enable diagnostics at start
          status_diagnostics_enabled = true, -- enable diagnostics in statusline
-         icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-         ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+         icons_enabled = true,              -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+         ui_notifications_enabled = true,   -- disable notifications when toggling UI elements
       },
    },
    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
@@ -72,14 +72,14 @@ local config = {
       servers = {},
       formatting = {
          format_on_save = {
-            enabled = true, -- enable or disable format on save globally
-            allow_filetypes = { -- enable format on save for specified filetypes only
+            enabled = true,      -- enable or disable format on save globally
+            allow_filetypes = {  -- enable format on save for specified filetypes only
             },
             ignore_filetypes = { -- disable format on save for specified filetypes
                "markdown",
             },
          },
-         disabled = { -- disable formatting capabilities for the listed language servers
+         disabled = {       -- disable formatting capabilities for the listed language servers
          },
          timeout_ms = 1000, -- default format timeout
       },
@@ -95,7 +95,8 @@ local config = {
          ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
          ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
          -- dap
-         ["<leader>d"] = { function() end, desc = "+Debug" },
+         ["<leader>d"] = { function()
+         end, desc = "+Debug" },
          -- ["<leader>dd"] = { "<cmd>DapToggleBreakpoint<cr>", desc = "Toggle breakpoint" },
          -- ["<leader>dc"] = { "<cmd>DapContinue<cr>", desc = "Continue" },
          -- ["<leader>di"] = { "<cmd>DapStepInto<cr>", desc = "Step into" },
@@ -119,8 +120,7 @@ local config = {
             function()
                if vim.bo.filetype == "typescript" or vim.bo.filetype == "typescriptreact" then
                   vim.cmd([[TypescriptOrganizeImports]])
-               elseif vim.bo.filetype == "scala" then
-                  vim.cmd("MetalsOrganizeImports")
+                  vim.cmd([[TypescriptRemoveUnused]])
                else
                   vim.notify("Unsupported filetype.")
                end
@@ -157,7 +157,7 @@ local config = {
    -- Configure plugins
    plugins = {
       ["p00f/nvim-ts-rainbow"] = { enabled = false },
-      { "astrotheme", enabled = false },
+      { "astrotheme",             enabled = false },
       -- { "rktjmp/shipwright.nvim", lazy = false },
       {
          "mcchrish/zenbones.nvim",
@@ -170,6 +170,11 @@ local config = {
       {
          dir = "~/workspace/rose-pine",
          name = "rose-pine",
+         config = function()
+            require("rose-pine").setup({
+               dark_variant = 'moon',
+            })
+         end
       },
       {
          "goolord/alpha-nvim",
@@ -254,17 +259,16 @@ local config = {
          }
          return config -- return final config table
       end,
-      treesitter = { -- overrides `require("treesitter").setup(...)`
+      treesitter = {   -- overrides `require("treesitter").setup(...)`
          -- ensure_installed = { "lua" },
       },
-      { "scalameta/nvim-metals", requires = { "nvim-lua/plenary.nvim" } },
       ["mason-lspconfig"] = { -- overrides `require("mason-lspconfig").setup(...)`
          ensure_installed = { "sumneko_lua", "tsserver", "rust_analyzer", "sourcekit" },
       },
       ["mason-null-ls"] = { -- overrides `require("mason-null-ls").setup(...)`
          ensure_installed = { "prettier", "stylua" },
       },
-      { "sindrets/diffview.nvim", lazy = false, dependencies = { "nvim-lua/plenary.nvim" } },
+      { "sindrets/diffview.nvim", lazy = false,   dependencies = { "nvim-lua/plenary.nvim" } },
    },
    luasnip = {
       -- Extend filetypes
@@ -296,16 +300,6 @@ local config = {
                root_dir = require("lspconfig.util").root_pattern("Pneumatic.xcodeproj", ".git"),
             })
             require("xbase").setup({}) -- see default configuration bellow
-         end,
-      })
-
-      vim.api.nvim_create_augroup("metals", {})
-      vim.api.nvim_create_autocmd("FileType", {
-         pattern = "scala",
-         desc = "load metals",
-         group = "metals",
-         callback = function()
-            require("user.plugin.metals")
          end,
       })
 
