1dd5a978a44410915c8ebe94f89951f8a84a79af
Christopher Kahn <92762809+kahnclusions@users.noreply.github.com>
2024-03-28 10:52:05 +0800

    fix some typescript-tools issues, add conform

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -14,16 +14,16 @@ end
 
 local config = {
    updater = {
-      remote = "origin",     -- remote to use
-      channel = "stable",    -- "stable" or "nightly"
-      version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-      branch = "nightly",    -- branch name (NIGHTLY ONLY)
-      commit = nil,          -- commit hash (NIGHTLY ONLY)
-      pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
-      skip_prompts = false,  -- skip prompts about breaking changes
+      remote = "origin", -- remote to use
+      channel = "stable", -- "stable" or "nightly"
+      version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+      branch = "nightly", -- branch name (NIGHTLY ONLY)
+      commit = nil, -- commit hash (NIGHTLY ONLY)
+      pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
+      skip_prompts = false, -- skip prompts about breaking changes
       show_changelog = true, -- show the changelog after performing an update
-      auto_reload = false,   -- automatically reload and sync packer after a successful update
-      auto_quit = false,     -- automatically quit the current session after a successful update
+      auto_reload = false, -- automatically reload and sync packer after a successful update
+      auto_quit = false, -- automatically quit the current session after a successful update
    },
    lazy = {
       lockfile = vim.fn.stdpath("config") .. "/../astronvim/lazy-lock.json",
@@ -53,21 +53,21 @@ local config = {
       opt = {
          -- set to true or false etc.
          relativenumber = false, -- sets vim.opt.relativenumber
-         number = false,         -- sets vim.opt.number
-         spell = false,          -- sets vim.opt.spell
+         number = false, -- sets vim.opt.number
+         spell = false, -- sets vim.opt.spell
          -- signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-         wrap = true,            -- sets vim.opt.wrap
+         wrap = true, -- sets vim.opt.wrap
          showtabline = 0,
       },
       g = {
-         mapleader = " ",                   -- sets vim.g.mapleader
-         autoformat_enabled = true,         -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-         cmp_enabled = true,                -- enable completion at start
-         autopairs_enabled = true,          -- enable autopairs at start
-         diagnostics_enabled = true,        -- enable diagnostics at start
+         mapleader = " ", -- sets vim.g.mapleader
+         autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+         cmp_enabled = true, -- enable completion at start
+         autopairs_enabled = true, -- enable autopairs at start
+         diagnostics_enabled = true, -- enable diagnostics at start
          status_diagnostics_enabled = true, -- enable diagnostics in statusline
-         icons_enabled = true,              -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-         ui_notifications_enabled = true,   -- disable notifications when toggling UI elements
+         icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+         ui_notifications_enabled = true, -- disable notifications when toggling UI elements
          indent_blankline_show_current_context = true,
       },
    },
@@ -78,14 +78,13 @@ local config = {
    },
    lsp = {
       setup_handlers = {
-         tsserver = function()
-         end
+         tsserver = function() end,
          -- tsserver = function(_, opts)
          --    require("typescript").setup({ server = opts })
          -- end,
       },
       config = {
-         vtsls = function() 
+         vtsls = function()
             return require("vtsls").lspconfig
          end,
          ["typescript-tools"] = { -- enable inlay hints by default for `typescript-tools`
@@ -95,39 +94,39 @@ local config = {
                tsserver_max_memory = "auto",
                code_lens = "all",
                tsserver_file_preferences = {
-                 includeInlayParameterNameHints = "all",
-                 includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                 includeInlayFunctionParameterTypeHints = true,
-                 includeInlayVariableTypeHints = true,
-                 includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-                 includeInlayPropertyDeclarationTypeHints = true,
-                 includeInlayFunctionLikeReturnTypeHints = true,
-                 includeInlayEnumMemberValueHints = true,
+                  includeInlayParameterNameHints = "all",
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                  includeInlayFunctionParameterTypeHints = true,
+                  includeInlayVariableTypeHints = true,
+                  includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+                  includeInlayPropertyDeclarationTypeHints = true,
+                  includeInlayFunctionLikeReturnTypeHints = true,
+                  includeInlayEnumMemberValueHints = true,
                },
                tsserver_plugins = {
-                 "@styled/typescript-styled-plugin",
+                  "@styled/typescript-styled-plugin",
                },
                expose_as_code_action = "all",
                jsx_close_tag = {
-                 enable = true,
-                 filetypes = { "javascriptreact", "typescriptreact" },
+                  enable = true,
+                  filetypes = { "javascriptreact", "typescriptreact" },
                },
             },
-         }
+         },
       },
       skip_setup = {},
       servers = {},
       formatting = {
          format_on_save = {
-            enabled = true,      -- enable or disable format on save globally
-            allow_filetypes = {  -- enable format on save for specified filetypes only
+            enabled = false, -- enable or disable format on save globally
+            allow_filetypes = { -- enable format on save for specified filetypes only
             },
             ignore_filetypes = { -- disable format on save for specified filetypes
                "markdown",
             },
          },
-         disabled = {       -- disable formatting capabilities for the listed language servers
-            "tsserver"
+         disabled = { -- disable formatting capabilities for the listed language servers
+            "tsserver",
          },
          timeout_ms = 1000, -- default format timeout
       },
@@ -215,7 +214,7 @@ local config = {
          },
       },
       ["p00f/nvim-ts-rainbow"] = { enabled = false },
-      { "astrotheme",             enabled = false },
+      { "astrotheme", enabled = false },
       -- { "rktjmp/shipwright.nvim", lazy = false },
       {
          "mcchrish/zenbones.nvim",
@@ -286,7 +285,7 @@ local config = {
       {
          "goolord/alpha-nvim",
          config = function()
-            require 'alpha'.setup(require 'alpha.themes.theta'.config)
+            require("alpha").setup(require("alpha.themes.theta").config)
          end,
          opts = function(_, opts)
             opts.section.header.val = {
@@ -306,7 +305,7 @@ local config = {
          end,
          keys = require("user.plugin.hop").keys,
       },
-      { 'yioneko/nvim-vtsls', requires = { 'neovim/nvim-lspconfig' } },
+      { "yioneko/nvim-vtsls", requires = { "neovim/nvim-lspconfig" } },
       {
          "pmizio/typescript-tools.nvim",
          dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
@@ -314,13 +313,20 @@ local config = {
          -- get AstroLSP provided options like `on_attach` and `capabilities`
          opts = function()
             local astrolsp_avail, astrolsp = pcall(require, "astrolsp")
-            if astrolsp_avail then return astrolsp.lsp_opts "typescript-tools" end
-          end,
-          keys = {
-            { "<leader>lu", "<cmd>TSToolsRemoveUnusedImports<cr>",     desc = "Remove unused statements" },
-            { "<leader>lU", "<cmd>TSToolsRemoveUnused<cr>",     desc = "Remove unused statements" },
-            { "<leader>li", "<cmd>TSToolsAddMissingImports<cr>",     desc = "Remove unused statements" },
-          }
+            if astrolsp_avail then
+               return astrolsp.lsp_opts("typescript-tools")
+            end
+         end,
+         keys = {
+            { "<leader>lu", "<cmd>TSToolsRemoveUnusedImports<cr>", desc = "Remove unused statements" },
+            { "<leader>lU", "<cmd>TSToolsRemoveUnused<cr>", desc = "Remove unused statements" },
+            { "<leader>li", "<cmd>TSToolsAddMissingImports<cr>", desc = "Remove unused statements" },
+            { "<leader>ld", vim.lsp.buf.definition, desc = "Go to Definition " },
+            { "gd", vim.lsp.buf.definition, desc = "Go to Definition " },
+         },
+      },
+      {
+         "stevearc/conform.nvim",
       },
       -- {
       --    "cormacrelf/dark-notify",
@@ -374,7 +380,7 @@ local config = {
             extensions = {
                smart_open = {
                   show_scores = true,
-                  open_buffer_indicators = {previous = "👀", others = "🙈"},
+                  open_buffer_indicators = { previous = "👀", others = "🙈" },
                },
                file_browser = {
                   -- theme = "ivy",
@@ -417,24 +423,24 @@ local config = {
                            "Binary cannot be previewed"
                         )
                      end
-                  end
-               }
-            }
+                  end,
+               },
+            },
          },
          keys = {
             -- goto
-            { "<leader>fd", "<cmd>Telescope lsp_definitions<cr>",     desc = "Go to definition" },
-            { "<leader>fr", "<cmd>Telescope lsp_references<cr>",      desc = "Go to references" },
+            { "<leader>fd", "<cmd>Telescope lsp_definitions<cr>", desc = "Go to definition" },
+            { "<leader>fr", "<cmd>Telescope lsp_references<cr>", desc = "Go to references" },
             { "<leader>fi", "<cmd>Telescope lsp_implementations<cr>", desc = "Go to implementations" },
-            { 
-               "<leader>fe", 
-               function () 
+            {
+               "<leader>fe",
+               function()
                   require("telescope").extensions.smart_open.smart_open()
-               end,          
-               desc = "Colorscheme" 
+               end,
+               desc = "Colorscheme",
             },
-            { "<leader>aa", "<cmd>AerialToggle<cr>",                  desc = "Aerial Toggle" },
-            { "<leader>sa", "<cmd>Telescope aerial<cr>",              desc = "Colorscheme" },
+            { "<leader>aa", "<cmd>AerialToggle<cr>", desc = "Aerial Toggle" },
+            { "<leader>sa", "<cmd>Telescope aerial<cr>", desc = "Colorscheme" },
             -- search
             -- { "sb", "<cmd>Telescope git_branches<cr>", desc = "Checkout branch" },
             {
@@ -442,18 +448,18 @@ local config = {
                "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>",
                desc = "File browser",
             },
-            { "<leader>ss", "<cmd>Telescope smart_open<cr>",  desc = "Smart open" },
+            { "<leader>ss", "<cmd>Telescope smart_open<cr>", desc = "Smart open" },
             { "<leader>sc", "<cmd>Telescope colorscheme<sr>", desc = "Colorscheme" },
-            { "<leader>sh", "<cmd>Telescope help_tags<cr>",   desc = "Find Help" },
-            { "<leader>sM", "<cmd>Telescope man_pages<cr>",   desc = "Man Pages" },
-            { "<leader>sr", "<cmd>Telescope oldfiles<cr>",    desc = "Open Recent File" },
-            { "<leader>sR", "<cmd>Telescope registers<cr>",   desc = "Registers" },
-            { "<leader>sk", "<cmd>Telescope keymaps<cr>",     desc = "Keymaps" },
-            { "<leader>sC", "<cmd>Telescope commands<cr>",    desc = "Commands" },
-            { "<leader>sH", "<cmd>Telescope highlights<cr>",  desc = "Highlight Groups" },
+            { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Find Help" },
+            { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
+            { "<leader>sr", "<cmd>Telescope oldfiles<cr>", desc = "Open Recent File" },
+            { "<leader>sR", "<cmd>Telescope registers<cr>", desc = "Registers" },
+            { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
+            { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
+            { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Highlight Groups" },
 
-            { "<leader>lr", vim.lsp.buf.rename, desc = "Rename"},
-            { "<leader>lR", vim.lsp.buf.references, desc = "References"},
+            { "<leader>lr", vim.lsp.buf.rename, desc = "Rename" },
+            { "<leader>lR", vim.lsp.buf.references, desc = "References" },
          },
          -- opts = function()
          --    return {
@@ -485,7 +491,7 @@ local config = {
          }
          return config -- return final config table
       end,
-      treesitter = {   -- overrides `require("treesitter").setup(...)`
+      treesitter = { -- overrides `require("treesitter").setup(...)`
          -- ensure_installed = { "lua" },
       },
       ["mason-lspconfig"] = { -- overrides `require("mason-lspconfig").setup(...)`
@@ -494,7 +500,7 @@ local config = {
       ["mason-null-ls"] = { -- overrides `require("mason-null-ls").setup(...)`
          ensure_installed = { "prettier", "stylua" },
       },
-      { "sindrets/diffview.nvim", lazy = false,   dependencies = { "nvim-lua/plenary.nvim" } },
+      { "sindrets/diffview.nvim", lazy = false, dependencies = { "nvim-lua/plenary.nvim" } },
    },
    luasnip = {
       -- Extend filetypes
@@ -531,6 +537,31 @@ local config = {
 
       require("telescope").load_extension("file_browser")
       require("telescope").load_extension("smart_open")
+
+      require("conform").setup({
+         format_on_save = {
+            -- These options will be passed to conform.format()
+            timeout_ms = 500,
+            lsp_fallback = false,
+         },
+         notify_on_error = true,
+         formatters_by_ft = {
+            lua = { "stylua" },
+            -- Conform will run multiple formatters sequentially
+            python = { "isort", "black" },
+            -- Use a sub-list to run only the first available formatter
+            javascript = { { "prettier" } },
+            typescript = { { "prettier" } },
+            typescriptreact = { { "prettier" } },
+         },
+      })
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+         pattern = "*",
+         callback = function(args)
+            require("conform").format({ bufnr = args.buf })
+         end,
+      })
 
       -- vim.api.nvim_create_augroup("js-debug", {})
       -- vim.api.nvim_create_autocmd("FileType", {
