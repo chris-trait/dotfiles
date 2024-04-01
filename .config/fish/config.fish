a0ed32fb197e67c254e7b61e78ad44025b90f5fe
Christopher Kahn <92762809+kahnclusions@users.noreply.github.com>
2024-04-01 11:32:18 +0800

    Cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -47,6 +47,12 @@ function update_theme --on-variable macOS_Theme
     end
 end
 
+set -Ux FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS \
+    --color=fg:#c0caf5,bg:#24283b,hl:#ff9e64 \
+    --color=fg+:#c0caf5,bg+:#292e42,hl+:#ff9e64 \
+    --color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff \
+    --color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a"
+
 set system_theme (defaults read -g AppleInterfaceStyle 2> /dev/null)
 
 if [ "$system_theme" = "Dark" ];
